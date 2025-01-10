8e1da7117d4d9f7e904dd7d46f2c0526f03afe9a
Alvin <fatman01923@yahoo.com>
2025-01-10 16:25:05 -0500

    Added the script

---
+++ radarr_convert_csv_to_sql.py
@@ -0,0 +1,44 @@
+import pandas, csv
+import os
+from io import StringIO
+from sqlalchemy import create_engine
+
+def psql_insert_copy(table, conn, keys, data_iter):
+    dbapi_conn = conn.connection
+    with dbapi_conn.cursor() as cur:
+        s_buf = StringIO()
+        writer = csv.writer(s_buf)
+        writer.writerows(data_iter)
+        s_buf.seek(0)
+        columns = ', '.join('"{}"'.format(k) for k in keys)
+        if table.schema:
+            table_name = '{}.{}'.format(table.schema, table.name)
+        else:
+            table_name = table.name
+        sql = 'COPY {} ({}) FROM STDIN WITH CSV'.format(table_name, columns)
+        cur.copy_expert(sql=sql, file=s_buf)
+
+engine = create_engine('postgresql://username:password@127.0.0.1:5432/radarr-main')
+
+
+def main():
+    directory = os.getcwd()
+    for filename in os.listdir(directory):
+        if filename.endswith('.csv'):
+            with open(os.path.join(directory, filename), encoding="utf-8") as csv_file:
+                df = pandas.read_csv(csv_file)
+                df.set_index(df.columns[0], inplace=True)
+                table_name = '"' + filename[:-4] + '"'
+                print(table_name)
+                if table_name == '"MovieMetadata"':
+                    df["SecondaryYear"] = df["SecondaryYear"].astype(pandas.Int64Dtype())
+                try:
+                    df.to_sql(table_name, engine, schema='public', method=psql_insert_copy, if_exists='replace')
+                except Exception as e:
+                    print("The table ", table_name, "failed to update")
+                    print(e)
+                    break
+
+if __name__ == "__main__":
+    main()
+
